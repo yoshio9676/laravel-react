0ef4118183b3a5af57609238ce7d2be79081c81d
yoshi9676 <yoshitarou0706@gmail.com>
2024-03-28 17:03:54 +0900

    sample page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SampleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('sample', [SampleController::class, 'index'])->name('sample.index');
